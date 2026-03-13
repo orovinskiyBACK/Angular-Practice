6ef40fb69322aa1f80f38052b012f87255653a83
OrovinskiyBack <olegrovin22@proton.me>
2026-03-12 20:41:42 -0700

    Added connection to local db with entity schema as well as a controller. Leaving example app for examples

---
+++ api/CodePulse.API/CodePulse.API/Models/Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
