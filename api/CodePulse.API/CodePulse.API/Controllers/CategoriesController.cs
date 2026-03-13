2a9201215b13bc6ae900d718a7a8a736659848d4
OrovinskiyBack <olegrovin22@proton.me>
2026-03-12 21:13:27 -0700

    implmented repository patters for categories

--- api/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ api/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -1,11 +1,44 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
 {
+    // https://localhost:xxxx/api/categories
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map Dto to Domain Model
+            var category = new Category
+            {
+                name = request.name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            // Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.name,
+                UrlHandle = category.UrlHandle
+            };
+            return Ok(response);
+        }
     }
 }
---
+++ api/CodePulse.API/CodePulse.API/Models/DTO/CreateCategoryRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CodePulse.API.Models.DTO
+{
+    public class CreateCategoryRequestDto
+    {
+        public string name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
